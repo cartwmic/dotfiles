6154b3fb4374cd837cd7be75ef1fba74e4442510
Michael Cartwright <cartwmic@gmail.com>
2023-03-07 23:39:18 -0800

    treesitter plugin; kitty theme change; zellij initial setup

---
+++ dotfiles/config/nvim/lua/plugins/treesitter.lua
@@ -0,0 +1,27 @@
+return {
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = {
+      ensure_installed = {
+        "bash",
+        "c",
+        "help",
+        "html",
+        "javascript",
+        "json",
+        "kdl",
+        "lua",
+        "luap",
+        "markdown",
+        "markdown_inline",
+        "python",
+        "query",
+        "regex",
+        "tsx",
+        "typescript",
+        "vim",
+        "yaml",
+      },
+    },
+  },
+}
