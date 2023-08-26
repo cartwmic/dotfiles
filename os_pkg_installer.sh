b8819e73c98ac923b718599e61928f1a1b33ea63
Michael Cartwright <cartwmic@gmail.com>
2023-08-26 19:38:51 +0000

    add kustomize/helm install; fix tasks install

--- os_pkg_installer.sh
+++ os_pkg_installer.sh
@@ -7,11 +7,11 @@ if [ "$2" = 'sdkman' ]; then # install via install script regardless of os
 elif [ "$1" = 'macos' ]; then
 	brew install "$2"
 elif [ "$1" = 'ubuntu' ]; then
-	if [ "$2" = 'go-task' ] || [ "$2" = 'lazygit' ] || [ "$2" = 'starship' ] || [ "$2" = 'zellij' ]; then # install via install script
+	if [ "$2" = 'go-task' ] || [ "$2" = 'lazygit' ] || [ "$2" = 'starship' ] || [ "$2" = 'zellij' ] || [ "$2" = 'kustomize' ]; then # install via install script
 		if [ "$2" = 'go-task' ]; then
-			sudo sh -c "$(curl --location https://taskfile.dev/install.sh)" -- -d
+			sudo -u cartwmic sh -c "$(curl --location https://taskfile.dev/install.sh)" -- -d -b "$HOME/.local/bin"
 		elif [ "$2" = 'lazygit' ]; then
-			cd $HOME || exit
+			cd "$HOME" || exit
 			mkdir "lazygit"
 			LAZYGIT_VERSION=$(curl -s "https://api.github.com/repos/jesseduffield/lazygit/releases/latest" | grep -Po '"tag_name": "v\K[^"]*')
 			curl -Lo "lazygit.tar.gz" "https://github.com/jesseduffield/lazygit/releases/latest/download/lazygit_${LAZYGIT_VERSION}_Linux_x86_64.tar.gz"
@@ -21,6 +21,10 @@ elif [ "$1" = 'ubuntu' ]; then
 			curl -sS https://starship.rs/install.sh | sudo -u cartwmic sh
 		elif [ "$2" = 'zellij' ]; then
 			cargo install --locked zellij
+		elif [ "$2" = 'kustomize' ]; then
+			cd "$HOME/.local/bin" || exit
+			curl -s "https://raw.githubusercontent.com/kubernetes-sigs/kustomize/master/hack/install_kustomize.sh" | bash
+			cd - || exit
 		fi
 	else # install via apt
 		if [ "$2" = 'kubectl' ]; then
@@ -33,7 +37,11 @@ elif [ "$1" = 'ubuntu' ]; then
 			sudo apt-get install software-properties-common
 			sudo add-apt-repository ppa:neovim-ppa/unstable
 			sudo apt-get update
-			sudo apt-get install neovim
+		elif [ "$2" = 'helm' ]; then
+			curl https://baltocdn.com/helm/signing.asc | gpg --dearmor | sudo tee /usr/share/keyrings/helm.gpg >/dev/null
+			sudo apt-get install apt-transport-https --yes
+			echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/helm.gpg] https://baltocdn.com/helm/stable/debian/ all main" | sudo tee /etc/apt/sources.list.d/helm-stable-debian.list
+			sudo apt-get update
 		fi
 		sudo apt install -y "$2"
 	fi
