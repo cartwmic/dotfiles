d8d9873a3fec3e7db6d7e5dedcf69b6d11b8b69e
Michael Cartwright <cartwmic@gmail.com>
2023-10-18 19:04:14 -0700

    fix lazygit config templating; update lazy lock; update lazygit config dotdrop key

--- dotfiles/config/nvim/lua/plugins/lualine.lua
+++ dotfiles/config/nvim/lua/plugins/lualine.lua
@@ -1,4 +1,3 @@
-
 -- As of 2023-09-25 I couldn't get this to work because line 12 doesn't work for some reason
 -- lualine comes with defaults in ~/.local/share/nvim/lazy/LazyVim/lua/lazyvim/plugins/ui.lua
 return {
@@ -9,9 +8,13 @@ return {
     opts = function(_, opts)
       local Util = require("lazyvim.util")
       table.insert(opts.sections.lualine_x, 3, {
-        function() return vim.g["metals_status"] or "metals_status not enabled in nvim-metals config" end,
-        cond = function () return package.loaded["metals"] end,
-        color = Util.fg("Debug"),
+        function()
+          return vim.g["metals_status"] or "metals_status not enabled in nvim-metals config"
+        end,
+        cond = function()
+          return package.loaded["metals"]
+        end,
+        color = Util.ui.fg("Debug"),
       })
     end,
   },
