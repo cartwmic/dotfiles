d9de0d9a6aed9d2f5e33091b58ebc1ab763e3f37
Michael Cartwright <cartwmic@gmail.com>
2024-09-29 19:56:32 -0700

    update lock file; add extras; remove deprecated ls for yaml

--- dotfiles/config/nvim/lua/plugins/lsp.lua
+++ dotfiles/config/nvim/lua/plugins/lsp.lua
@@ -8,30 +8,6 @@ return {
       -- return true if you don't want this server to be setup with lspconfig
       ---@type table<string, fun(server:string, opts:_.lspconfig.options):boolean?>
       setup = {
-        yamlls = function(_, _)
-          require("lspconfig").yamlls.setup({
-            on_attach = function(client, buffer)
-              -- for some reason this is set to false even though yamlls supports it
-              if client.name == "yamlls" then
-                client.server_capabilities.documentFormattingProvider = true
-              end
-              -- need to redo the side effects of these on_attach since I've updated server_capabilities
-              -- and they depend on the values of server_capabilities
-              -- require("lazyvim.plugins.lsp.format").on_attach(client, buffer)
-              require("lazyvim.plugins.lsp.keymaps").on_attach(client, buffer)
-              on_attach(client, buffer)
-            end,
-            settings = {
-              yaml = {
-                keyOrdering = false,
-                format = {
-                  enable = true,
-                },
-              },
-            },
-          })
-          return true
-        end,
         html = function(_, _)
           --Enable (broadcasting) snippet capability for completion
           local capabilities = vim.lsp.protocol.make_client_capabilities()
