68a3f41fd35f561164aa6e856021d22793d07925
Michael Cartwright <cartwmic@gmail.com>
2024-11-12 23:50:44 -0800

    update font installation fix syntax errors

--- os_fonts_installer.sh
+++ os_fonts_installer.sh
@@ -2,22 +2,22 @@
 
 ret=0
 if [ "$1" = 'true' ]; then
-	echo "Skip fonts flag set to true, exit 0..."
+  echo "Skip fonts flag set to true, exit 0..."
 elif [ "$2" = 'macos' ]; then
-	echo "macos OS selected"
-	brew tap homebrew/cask-fonts && brew install --cask font-sauce-code-pro-nerd-font
-	ret=$?
+  echo "macos OS selected"
+  brew install --cask font-sauce-code-pro-nerd-font
+  ret=$?
 elif [ "$2" = 'ubuntu' ]; then
-	echo "ubuntu OS selected"
-	cd /tmp || exit
-	curl -o SourceCodePro.zip https://github.com/ryanoasis/nerd-fonts/releases/download/v3.0.2/SourceCodePro.zip
-	unzip SourceCodePro.zip
-	mv ./SourceCodePro/*ttf ~/.local/share/fonts
-	fc-cache -fv
-	cd - || exit
-	ret=$?
+  echo "ubuntu OS selected"
+  cd /tmp || exit
+  curl -o SourceCodePro.zip https://github.com/ryanoasis/nerd-fonts/releases/download/v3.0.2/SourceCodePro.zip
+  unzip SourceCodePro.zip
+  mv ./SourceCodePro/*ttf ~/.local/share/fonts
+  fc-cache -fv
+  cd - || exit
+  ret=$?
 else
-	echo "os: '$2' is not supported"
+  echo "os: '$2' is not supported"
 fi
 
 printf "\xf0\x9f\x90\x8d\n"
