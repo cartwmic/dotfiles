07496cc270c2fe1617d141d636f666962f67af83
cartwmic-g14-wsl2 <cartwmic@gmail.com>
2025-03-16 20:59:31 -0700

    add timestamp user command

--- dotfiles/config/nvim/init.lua
+++ dotfiles/config/nvim/init.lua
@@ -1,3 +1,10 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 
 require("config.lazy")
+
+-- User Commands
+vim.api.nvim_create_user_command("User_InsertPKMSFileTimestamp", function(_)
+  local time = vim.fn.strftime("%Y-%m-%d-%H%M%S")
+  local row, col = unpack(vim.api.nvim_win_get_cursor(0))
+  vim.api.nvim_buf_set_text(0, row - 1, col, row - 1, col, { time })
+end, {})
