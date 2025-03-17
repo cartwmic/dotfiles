285171491fe60ed2280e2e79f38a8286fd504a9b
cartwmic-g14-wsl2 <cartwmic@gmail.com>
2025-03-16 21:12:04 -0700

    update user command for timestamp

--- dotfiles/config/nvim/init.lua
+++ dotfiles/config/nvim/init.lua
@@ -3,8 +3,7 @@
 require("config.lazy")
 
 -- User Commands
-vim.api.nvim_create_user_command("User_InsertPKMSFileTimestamp", function(_)
+vim.api.nvim_create_user_command("UserPutPKMSFileTimestampToClipboard", function(_)
   local time = vim.fn.strftime("%Y-%m-%d-%H%M%S")
-  local row, col = unpack(vim.api.nvim_win_get_cursor(0))
-  vim.api.nvim_buf_set_text(0, row - 1, col, row - 1, col, { time })
+  vim.fn.setreg("+", time)
 end, {})
