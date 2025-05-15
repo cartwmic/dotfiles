06f0b39ed53b1bafae22236c26153ea480db6588
mcartwright <mcartwright@axon.com>
2025-05-15 10:10:29 -0700

    add user leader z commands

--- dotfiles/config/nvim/init.lua
+++ dotfiles/config/nvim/init.lua
@@ -3,7 +3,12 @@
 require("config.lazy")
 
 -- User Commands
-vim.api.nvim_create_user_command("UserPutPKMSFileTimestampToClipboard", function(_)
+vim.api.nvim_create_user_command("UserPutPKMSFileDateTimestampToClipboard", function(_)
   local time = vim.fn.strftime("%Y-%m-%d-%H%M%S")
+  vim.fn.setreg("+", time)
+end, {})
+
+vim.api.nvim_create_user_command("UserPutCurrentTimestampToClipboard", function(_)
+  local time = vim.fn.strftime("%H:%M:%S")
   vim.fn.setreg("+", time)
 end, {})
--- dotfiles/config/nvim/lua/config/keymaps.lua
+++ dotfiles/config/nvim/lua/config/keymaps.lua
@@ -4,3 +4,15 @@
 vim.keymap.set("i", "jk", "<ESC>", { noremap = true, desc = "Escape insert mode" })
 vim.keymap.set("t", "<C-\\>", "<C-\\><C-n>", { noremap = true, desc = "Escape terminal mode" })
 vim.keymap.set("n", "<leader>dT", vim.lsp.codelens.run, { noremap = true, desc = "Run codelens" })
+vim.keymap.set(
+  "n",
+  "<leader>zt",
+  "<cmd>UserPutCurrentTimestampToClipboard<CR>p",
+  { noremap = true, desc = "Get local timestamp to clipboard and paste it" }
+)
+vim.keymap.set(
+  "n",
+  "<leader>zT",
+  "<cmd>UserPutPKMSFileDateTimestampToClipboard<CR>p",
+  { noremap = true, desc = "Get local date timestamp to clipboard and paste it" }
+)
