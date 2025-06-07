b767eba0add679264a137727abe2cf18601a683b
cartwmic-g14-wsl2 <cartwmic@gmail.com>
2025-06-07 16:31:12 -0700

    update dotdrop; update text case commands

--- dotfiles/config/nvim/lua/plugins/text-case.lua
+++ dotfiles/config/nvim/lua/plugins/text-case.lua
@@ -7,8 +7,8 @@ return {
       require("telescope").load_extension("textcase")
     end,
     keys = {
-      "ga", -- Default invocation prefix
-      { "ga.", "<cmd>TextCaseOpenTelescope<CR>", mode = { "n", "x" }, desc = "Telescope" },
+      "<leader>za", -- Default invocation prefix
+      { "<leader>za.", "<cmd>TextCaseOpenTelescope<CR>", mode = { "n", "x" }, desc = "Telescope" },
     },
     cmd = {
       -- NOTE: The Subs command name can be customized via the option "substitude_command_name"
