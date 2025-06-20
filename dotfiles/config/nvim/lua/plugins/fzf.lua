e5f24a0ff9cb83ace6c3cde6fc7e9f380c6da1e5
cartwmic-g14-wsl2 <cartwmic@gmail.com>
2025-06-20 14:19:08 -0700

    add command for fzf lua normal mode

--- dotfiles/config/nvim/lua/plugins/fzf.lua
+++ dotfiles/config/nvim/lua/plugins/fzf.lua
@@ -3,6 +3,9 @@ local fzf = require("fzf-lua")
 return {
   {
     "ibhagwan/fzf-lua",
+    keys = {
+      { "<c-o>", "<c-\\><c-n>", ft = "fzf", mode = "t", nowait = true }, -- go to normal mode in fzf floating window (not for editing)
+    },
     opts = {
       grep = {
         actions = {
