bca139b36af074e2d572ec0c5433746333589b2e
cartwmic-g14-wsl2 <cartwmic@gmail.com>
2025-06-21 16:36:46 -0700

    fix version for lazygit

--- run_onchange_bootstrap.sh
+++ run_onchange_bootstrap.sh
@@ -122,7 +122,7 @@ install_ubuntu_special() {
       return 1
     fi
 
-    curl -Lo lazygit.tar.gz "https://github.com/jesseduffield/lazygit/releases/download/v${LAZYGIT_VERSION}/lazygit_${version}_Linux_x86_64.tar.gz"
+    curl -Lo lazygit.tar.gz "https://github.com/jesseduffield/lazygit/releases/download/v${version}/lazygit_${version}_Linux_x86_64.tar.gz"
     tar xf lazygit.tar.gz lazygit
     sudo install lazygit -D -t /usr/local/bin/
     ;;
