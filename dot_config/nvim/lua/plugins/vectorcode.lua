4b17302a7150c5ca4fd0ddd030b823d76fc6ddfd
cartwmic-g14-wsl2 <cartwmic@gmail.com>
2026-01-03 14:32:28 -0800

    udpate lazylock; update code companion for breaking changes

--- dot_config/nvim/lua/plugins/vectorcode.lua
+++ dot_config/nvim/lua/plugins/vectorcode.lua
@@ -1,5 +1,6 @@
 return {
   "Davidyz/VectorCode",
+  build = "uv tool upgrade vectorcode", -- This helps keeping the CLI up-to-date
   dependencies = { "nvim-lua/plenary.nvim" },
   cmd = "VectorCode", -- if you're lazy-loading VectorCode
 }
